af2d02981831616c00b11978f7934423ebf24bae
Andrew Patipaksiri <apax714@gmail.com>
2017-03-22 15:00:54 -0700

    constructor function example

---
+++ JSConcepts/ConstructorFn.js
@@ -0,0 +1,10 @@
+function Person(firstName, lastName, age, occupation) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+  this.job = occupation;
+}
+
+var andrew = new User('Andrew', 'Patipak', 28, 'Web Developer');
+
+andrew;
